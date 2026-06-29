d3018d10b5daa6f4ed7b586a8b485b37bfb448b0
mackliu <macklun@gmail.com>
2026-06-29 09:14:34 +0800

    建立主題內容頁面的頁籤功能需要的版型

---
+++ front/main.php
@@ -0,0 +1,44 @@
+<style>
+.tags {display: flex;padding-left: 1px;}
+
+.tags .tag {
+    border: 1px solid black;
+    width: 80px;
+    padding: 5px 10px;
+    text-align: center;
+    margin-left: -1px;
+}
+.posts{
+    margin-top:-1px;
+}
+.post {
+    border: 1px solid black;
+    min-height: 400px;
+    padding:10px;
+}
+
+.active{
+    display:block;
+}
+</style>
+<div class="tags">
+
+    <div class="tag">健康新知</div>
+    <div class="tag">菸害防制</div>
+    <div class="tag">癌症防治</div>
+    <div class="tag">慢性病防治</div>
+</div>
+<div class="posts">
+    <div class="post active">
+        <h3>健康新知</h3>
+    </div>
+    <div class="post">
+        <h3>菸害防制</h3>
+    </div>
+    <div class="post">
+        <h3>癌症防治</h3>
+    </div>
+    <div class="post">
+        <h3>慢性病防治</h3>
+    </div>
+</div>
