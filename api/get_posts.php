6eb2dbdec8d5792f6fe7e0a4fbe6aba90da16e51
mackliu <macklun@gmail.com>
2026-06-29 11:17:47 +0800

    完成點選分類文章標題,載入對應的完整文章內容

--- api/get_posts.php
+++ api/get_posts.php
@@ -1,8 +1,8 @@
 <?php include_once "db.php";
 
-$list=$News->all(['type'=>$_GET['text']]);
+$list=$News->all(['type'=>$_GET['type']]);
 foreach($list as $l){
-    echo "<a href='' style='margin:10px 0;display:block'>";
+    echo "<a href='javascript:getPost({$l['id']})' style='margin:10px 0;display:block'>";
     echo $l['title'];
     echo "</a>";
 }
